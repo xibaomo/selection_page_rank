39d428157c98b80fadb6af3fd3b26c492931ec9a
zhanghongyuand <14454203zhy@163.com>
2023-07-25 11:40:19 +0800

    Update Description: This update includes the following changes: Download the closing prices of S&P 500 stocks on a specified date, remove stocks with missing price data, update the CSV file after the removal, calculate the stationary distribution of the top 100 stocks based on the correlation matrix of the previous 180 days of S&P 500 data, save the ranking file, calculate the stock returns for the 30 days after the specified date, calculate the correlation between the ranking and the final returns for the selected stocks.

--- stock_page_rank.py
+++ stock_page_rank.py
@@ -3,12 +3,18 @@ import pandas as pd
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
-
-def calculate_correlation_matrix(stock_list_file, start_date, end_date):
+def download_sp500(stock_list_file, start_date, end_date):
     sp500 = pd.read_csv(stock_list_file)
     tickers = sp500['Symbol'].tolist()
     data = yf.download(tickers, start=start_date, end=end_date, interval="1d")
-    price_matrix = data['Close']
+    return data
+
+def calculate_correlation_matrix(sp500_data, start_date, end_date):
+    sp500 = pd.read_csv(stock_list_file)
+    start_date_timestamp = pd.to_datetime(start_date)
+    end_date_timestamp = pd.to_datetime(end_date)
+    previous_day = end_date_timestamp - pd.Timedelta(days=1)
+    price_matrix = sp500_data['Adj Close'].loc[start_date_timestamp:previous_day]
     price_matrix = price_matrix.dropna(thresh=10, axis=1)
     updated_tickers = price_matrix.columns.tolist()
     sp500_updated = sp500[sp500['Symbol'].isin(updated_tickers)]
@@ -40,20 +46,20 @@ def compute_stationary_distribution(trans_mat):
     v = row_one @ tmp
     return v[0, :]
 
-def calculate_relative_returns(stock_list_file, base_date, days_after):
+def calculate_relative_returns(sp500_data, base_date, days_after):
     # 下载多只股票的数据
     sp500 = pd.read_csv(stock_list_file)
     tickers = sp500['Symbol'].tolist()
-
-    # 创建一个空的DataFrame来存储每只股票的相对收益率
-    relative_returns_df = pd.DataFrame()
 
     # 将基准日期和未来日期转换为Timestamp对象
     base_date_timestamp = pd.Timestamp(base_date)
     end_date_timestamp = base_date_timestamp + pd.Timedelta(days=days_after)
 
-    # 下载所有股票基准日期和未来日期的数据
-    data = yf.download(tickers, start=base_date_timestamp, end=end_date_timestamp, interval="1d")
+    # 读取所有股票基准日期和未来日期的数据
+    data = sp500_data.loc[base_date_timestamp:end_date_timestamp]
+
+    # 创建一个空的DataFrame来存储每只股票的相对收益率
+    relative_returns_df = pd.DataFrame()
 
     # 遍历每只股票，计算收益率并将其添加到DataFrame
     for ticker in tickers:
@@ -63,7 +69,6 @@ def calculate_relative_returns(stock_list_file, base_date, days_after):
         relative_returns_df[ticker] = relative_returns  # 将收益率添加到DataFrame，并以股票代码作为列名
 
     return relative_returns_df
-
 
 
 if __name__ == "__main__":
@@ -78,24 +83,18 @@ if __name__ == "__main__":
 
     # 计算起始日期
     start_date = pd.to_datetime(target_date) - pd.Timedelta(days=historical_days)
+    end_date = pd.to_datetime(target_date) + pd.Timedelta(days=future_days)
 
-    # 下载目标日期的股票价格并更新CSV文件
-    target_data = yf.download(pd.read_csv(stock_list_file)['Symbol'].tolist(), start=target_date, end=target_date, interval="1d")
-    target_data = target_data['Close']
-
-    # 剔除没有价格的股票并更新CSV文件
-    updated_tickers = target_data.columns.dropna().tolist()
-    sp500_updated = pd.read_csv(stock_list_file).loc[pd.read_csv(stock_list_file)['Symbol'].isin(updated_tickers)]
-    sp500_updated.to_csv(stock_list_file, index=False)
-
+    sp500_data= download_sp500(stock_list_file, start_date, end_date)
+  
     # 计算相关性矩阵和稳态分布
     
-    corr_matrix = calculate_correlation_matrix(stock_list_file, start_date, target_date)
+    corr_matrix = calculate_correlation_matrix(sp500_data, start_date, target_date)
     v = compute_stationary_distribution(corr_matrix.values)
     syms = corr_matrix.index.values
     sort_id = np.argsort(v)[::-1]
-    score_sorted = v[sort_id]
-    syms_sorted = syms[sort_id]
+    score_sorted = v[sort_id][:100]
+    syms_sorted = syms[sort_id][:100]
 
     # 保存评分到CSV文件
     scores_df = pd.DataFrame({'Symbol': syms_sorted, 'Score': score_sorted})
@@ -103,14 +102,32 @@ if __name__ == "__main__":
 
     # 使用新函数计算收益率
     base_date = target_date
-    relative_returns_df = calculate_relative_returns(stock_list_file, base_date, future_days)
+    relative_returns_df = calculate_relative_returns(sp500_data, base_date, future_days)
 
     # 打印每只股票未来的最低、最高和总收益率
     for symbol in scores_df['Symbol']:
         min_return = relative_returns_df[symbol].min()
         max_return = relative_returns_df[symbol].max()
-        final_return = relative_returns_df[symbol].sum()
+        final_return = relative_returns_df[symbol].iloc[-1] 
         print(f"Symbol: {symbol}, Minimum_Return: {min_return:.4f}, Max_Return: {max_return:.4f}, Final_Return: {final_return:.4f}")
+
+    # 读取scores.csv文件
+    scores_df = pd.read_csv("scores.csv")
+
+    # 从relative_returns_df中提取对应的股票的最终收益率
+    final_returns = relative_returns_df.iloc[-1]
+
+    # 选取scores_df中'symbol'列对应的股票的得分
+    scores = scores_df.set_index('Symbol')['Score']
+    final_returns.rename('Return', inplace=True)
+    # 将scores和final_returns合并成一个DataFrame
+    scores_and_returns_df = pd.concat([scores, final_returns], axis=1)
+
+    # 计算得分和最终收益率的相关系数矩阵
+    correlation_matrix = scores_and_returns_df.corr()
+
+    print("Correlation Matrix between Score and Final Return for Top 100 Stocks:")
+    print(correlation_matrix)
 
     # 绘制所有股票评分的图表
     plt.stem(syms_sorted, score_sorted)
@@ -120,4 +137,6 @@ if __name__ == "__main__":
     plt.xticks(rotation=45)
     plt.tight_layout()
     plt.show()
- 
+
+   
+    
